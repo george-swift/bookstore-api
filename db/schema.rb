61d76fa6e426bc851c0b8e8e336abe988dd749c3
George <71434803+george-swift@users.noreply.github.com>
2021-07-16 17:19:12 +0100

    Change to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20_210_715_181_309) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension 'plpgsql'
+
   create_table 'books', force: :cascade do |t|
     t.string 'title'
     t.string 'author'
