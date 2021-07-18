1581a9dc28e32f9473f9e66869c4b9139d7eaa52
George <71434803+george-swift@users.noreply.github.com>
2021-07-18 20:23:34 +0100

    Add new columns to books table in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_185204) do
-
+ActiveRecord::Schema.define(version: 20_210_717_185_204) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
-    t.string "category"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "chapter"
-    t.bigint "percentage"
+  create_table 'books', force: :cascade do |t|
+    t.string 'title'
+    t.string 'author'
+    t.string 'category'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.bigint 'chapter'
+    t.bigint 'percentage'
   end
-
 end
